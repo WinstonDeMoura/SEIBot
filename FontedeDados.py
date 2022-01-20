c0f51e2941bc9e972812a8bc383d3ae53e874b4b
Winston De Moura <winston.de.moura@gmail.com>
2022-01-20 15:53:15 -0300

    Backend vs1

--- FontedeDados.py
+++ FontedeDados.py
@@ -2,12 +2,29 @@ import pandas as pd
 import re
 
 
+import pandas as pd
+import re
 
-tabela = pd.read_csv('Downloads/TRASH/AllStudentsExport.csv', encoding='latin-1', sep=';', usecols=['Nome', 'Número para Msg Text'])
+#Pega a base de dados do caminho e escolher quais colunas deseja ler
+tabela = pd.read_csv('ArquivosEssenciais/AllStudentsExport.csv', encoding='latin-1', sep=';', usecols=['Nome', 'Número para Msg Text'])
+
+#Renomeia o nome das colunas
 tabela.rename(columns={'Nome':'Alunos', 'Número para Msg Text':'Número'}, inplace=True)
+
+#Deixar só o primeiro nome
+n = tabela['Alunos']
+colunaalunos = [n.split(' ')[0] for n in n]
+###
+
+#Atualizar coluna alunos só com o primeiro nome
+tabela['Alunos'] = pd.DataFrame({'Nome':colunaalunos})
+
+
+#Colocando a tabela em ordem alfabetica
+tabela = tabela.sort_values('Alunos')
+
 ##Tira todos os campos vazios da tabela
 tabela.dropna(inplace=True)
 
-n = tabela.columns('Nome')
-print(n)
 
+tabela.to_excel("ArquivosEssenciais/BancoDeDados.xlsx",index=False)
